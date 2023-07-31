696f5dc1a1387803b8b0725f987869a216ff10c1
noumanHash <119563211+noumanHash@users.noreply.github.com>
2023-07-31 18:13:54 +0500

    Add files via upload

---
+++ src/app.test.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const REGISTER_API_URL = "http://localhost:3000/api/auth/register";
+const LOGIN_API_URL = "http://localhost:3000/api/auth/login";
+
+// Sample user data for testing
+const sampleUserData = {
+  email: "test6907@example.com",
+  password: "testpassword",
+};
+
+describe("Register API", () => {
+  it("registers a new user successfully", async () => {
+    try {
+      const response = await axios.post(REGISTER_API_URL, sampleUserData);
+      expect(response.status).toBe(200);
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  });
+  it("login a user successfully", async () => {
+    try {
+      const response = await axios.post(LOGIN_API_URL, sampleUserData);
+      expect(response.status).toBe(201);
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  });
+});
